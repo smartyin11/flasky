820db41f714883cae639f86781ed517dc7fa8fa4
Smart Yin <smartyin@qq.com>
2018-01-06 18:13:22 +0800

    修改

--- app/models.py
+++ app/models.py
@@ -9,14 +9,15 @@ class Role(db.Model):
     users = db.relationship('User', backref='role', lazy=True)
 
     def __repr__(self):
-        return '<Role %r>'%self.name
+        return '<Role %r>' % self.name
+
 
 class User(db.Model):
-    __tablename__='users'
+    __tablename__ = 'users'
 
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64),unique=True,index=True)
-    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
-        return '<User %r>'%self.username
+        return '<User %r>' % self.username
